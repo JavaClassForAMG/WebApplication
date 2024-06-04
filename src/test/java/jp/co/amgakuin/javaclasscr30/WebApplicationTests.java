8c7ac37a1ce3a62ecb1f0b5182d267fd62b87e90
Nobuyuki Kimura <a09@r-force.co.jp>
2024-06-04 18:52:25 +0900

    プロジェクト初期ファイル

---
+++ src/test/java/jp/co/amgakuin/javaclasscr30/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.amgakuin.javaclasscr30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
