3a2c816727148e861ed65f65a01e7725de280453
Hiroya Morozumi <123cr0013@tokyo.amg.ac.jp>
2024-06-17 16:24:53 +0900

    doPost関数でusernameパラメータを受け取る部分を追加

---
+++ src/main/java/jp/co/amgakuin/javaclasscr30/controller/FormPage.java
@@ -0,0 +1,17 @@
+package jp.co.amgakuin.javaclasscr30.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class FormPage {
+
+	@PostMapping("/")
+	private String doPost(
+			@RequestParam("username") String name
+			) {
+		System.out.println(name);
+		return "FormPage";
+	}
+}
