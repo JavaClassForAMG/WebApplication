aa82df642fbc7db5bbceeefca4066b7fbe678f7a
123cr0007 <123cr0007@tokyo.amg.ac.jp>
2024-06-17 14:43:43 +0900

    コメント追加

--- src/main/java/jp/co/amgakuin/javaclasscr30/WebApplication.java
+++ src/main/java/jp/co/amgakuin/javaclasscr30/WebApplication.java
@@ -3,9 +3,18 @@ package jp.co.amgakuin.javaclasscr30;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * 
+ * @author amg
+ *
+ */
 @SpringBootApplication
 public class WebApplication {
-
+	/**
+	 * 適当なコメント
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
