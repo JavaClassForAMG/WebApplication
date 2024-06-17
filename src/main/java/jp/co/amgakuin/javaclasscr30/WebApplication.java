6f28dc28861c4f294195b8d61651df83d45602ae
amg-n-kimura <n-kimura@tokyo.amg.ac.jp>
2024-06-17 14:45:17 +0900

    コメント追加

--- src/main/java/jp/co/amgakuin/javaclasscr30/WebApplication.java
+++ src/main/java/jp/co/amgakuin/javaclasscr30/WebApplication.java
@@ -3,9 +3,18 @@ package jp.co.amgakuin.javaclasscr30;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * WebApplicationのメインクラス
+ * @author amg
+ *
+ */
 @SpringBootApplication
 public class WebApplication {
 
+	/**
+	 * Springの起動メソッド
+	 * @param args 起動時の引数
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
