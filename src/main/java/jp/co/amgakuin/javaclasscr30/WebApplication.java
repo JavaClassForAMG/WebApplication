86bb8c431950514fb432f1dbcae749b3e5a5d9c1
jumokunoki <arakataenab@gmail.com>
2024-06-17 14:46:11 +0900

    コメント追加
    お試し版

--- src/main/java/jp/co/amgakuin/javaclasscr30/WebApplication.java
+++ src/main/java/jp/co/amgakuin/javaclasscr30/WebApplication.java
@@ -3,9 +3,20 @@ package jp.co.amgakuin.javaclasscr30;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * WebApplicationのメインクラス
+ *  
+ *
+ */
+
 @SpringBootApplication
 public class WebApplication {
 
+	/*
+	 * お疲れ様です～～～～。
+	 * 
+	 * 
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
