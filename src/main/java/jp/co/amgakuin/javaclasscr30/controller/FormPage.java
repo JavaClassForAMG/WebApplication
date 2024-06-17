2674ebbd238f2825d69580b24ff8ec6792935a89
kyohei fukuma <123cr0002@tokyo.amg.ac.jp>
2024-06-17 16:39:31 +0900

    名前を表示できるように

--- src/main/java/jp/co/amgakuin/javaclasscr30/controller/FormPage.java
+++ src/main/java/jp/co/amgakuin/javaclasscr30/controller/FormPage.java
@@ -1,6 +1,7 @@
 package jp.co.amgakuin.javaclasscr30.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -8,9 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FormPage 
 {
 	@PostMapping("/")
-	private String doPost(@RequestParam("username")String name) 
+	private String doPost(Model model,@RequestParam("username")String name) 
 	{
-		
+		model.addAttribute("name",name);
 		return "FormPage";
 	}
 
