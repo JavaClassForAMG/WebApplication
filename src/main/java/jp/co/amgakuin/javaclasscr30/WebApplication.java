5252741463d94f8f3de8369088ac331a3851874b
fukuma kyohei <123cr0002@tokyo.amg.ac.jp>
2024-06-17 14:43:43 +0900

    コメント追加

--- src/main/java/jp/co/amgakuin/javaclasscr30/WebApplication.java
+++ src/main/java/jp/co/amgakuin/javaclasscr30/WebApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebApplication {
 
+	/*Java授業 第二回*/
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
