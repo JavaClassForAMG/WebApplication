9483be63be0ef40a639121e3f5eddd32af4c32f8
amg-n-kimura <n-kimura@tokyo.amg.ac.jp>
2024-06-17 14:55:58 +0900

    トップページコントローラーのコメント追加

--- src/main/java/jp/co/amgakuin/javaclasscr30/controller/TopPage.java
+++ src/main/java/jp/co/amgakuin/javaclasscr30/controller/TopPage.java
@@ -4,9 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * トップページコントローラー
+ * @author amg
+ *
+ */
 @Controller
 public class TopPage {
 	
+	/**
+	 * インデックスページ表示
+	 * @param model
+	 * @return
+	 */
 	@GetMapping("/")
 	private String index(Model model) {
 		model.addAttribute("message", "Hello World!!");
