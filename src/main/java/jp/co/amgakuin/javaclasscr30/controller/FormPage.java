5b364864e365329ff20315eb36ec61095a157290
123cr0007 <123cr0007@tokyo.amg.ac.jp>
2024-06-17 16:23:31 +0900

    フォームデータの受信せ実装

--- src/main/java/jp/co/amgakuin/javaclasscr30/controller/FormPage.java
+++ src/main/java/jp/co/amgakuin/javaclasscr30/controller/FormPage.java
@@ -2,6 +2,7 @@ package jp.co.amgakuin.javaclasscr30.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormPage {
@@ -10,7 +11,11 @@ public class FormPage {
 	// 逆にURLから行くのはGet
 	// "/"が呼ばれたら舌の関数を呼ぶよって感じ
 	@PostMapping("/")
-	private String doPost() {
+	private String doPost(
+			@RequestParam("username") String name
+			) 
+	{
+		System out.println("name");
 		return "FormPage";
 	}
 }
