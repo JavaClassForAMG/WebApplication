c44d38e8985d5d1d60606c824100cefa2d864ac5
wagatsuma koudai <wagawaga3641@gmail.com>
2024-06-17 16:38:21 +0900

    FormPageのjavaとhtmlで名前が出るようになった
    名前が出るぞー！

--- src/main/java/jp/co/amgakuin/javaclasscr30/controller/FormPage.java
+++ src/main/java/jp/co/amgakuin/javaclasscr30/controller/FormPage.java
@@ -1,6 +1,7 @@
 package jp.co.amgakuin.javaclasscr30.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -8,9 +9,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FormPage {
 	
 	@PostMapping("/")
-	private String doPost(@RequestParam("username")String name) 
+	private String doPost
+	(
+		//引数
+		Model model,
+		@RequestParam("username")String username
+	) 
 	{
-		
+		//本文
+		//System.out.println(name);
+		model.addAttribute("name",username);
 		return "FormPage";
 	} 
 }
