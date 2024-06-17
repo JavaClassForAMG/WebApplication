05c24d2824c8a18342023c91e338c346ea706fe4
123cr0001 <123cr0001@tokyo.amg.ac.jp>
2024-06-17 14:50:00 +0900

    適当なコメントを追加

--- src/main/java/jp/co/amgakuin/javaclasscr30/WebApplication.java
+++ src/main/java/jp/co/amgakuin/javaclasscr30/WebApplication.java
@@ -2,10 +2,19 @@ package jp.co.amgakuin.javaclasscr30;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 適当
+ * @author amg
+ *
+ */
 
 @SpringBootApplication
 public class WebApplication {
 
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
