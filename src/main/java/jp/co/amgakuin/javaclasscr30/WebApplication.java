32f8ca336c8f019fd78e605a38e2826e91542596
wagatsuma koudai <wagawaga3641@gmail.com>
2024-06-17 14:43:46 +0900

    コメント入れた
    すっげー！

--- src/main/java/jp/co/amgakuin/javaclasscr30/WebApplication.java
+++ src/main/java/jp/co/amgakuin/javaclasscr30/WebApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebApplication {
 
+	//すっげー！勝手にコメントはいるぞぉー！
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
