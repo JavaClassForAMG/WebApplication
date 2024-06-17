5ded2286637d85cc1d02bb027e80135fe4b99432
kyohei fukuma <123cr0002@tokyo.amg.ac.jp>
2024-06-17 16:23:24 +0900

    フォームデータの受信

--- src/main/java/jp/co/amgakuin/javaclasscr30/controller/FormPage.java
+++ src/main/java/jp/co/amgakuin/javaclasscr30/controller/FormPage.java
@@ -2,13 +2,15 @@ package jp.co.amgakuin.javaclasscr30.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormPage 
 {
 	@PostMapping("/")
-	private String doPost() 
+	private String doPost(@RequestParam("username")String name) 
 	{
+		
 		return "FormPage";
 	}
 
