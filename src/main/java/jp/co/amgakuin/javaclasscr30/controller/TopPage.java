902289cc273e3d354dd596089348d1397c658587
byakuyarei0469 <siroku2580@gmail.com>
2024-06-17 14:56:02 +0900

    トップページコントローラにコメント記述

--- src/main/java/jp/co/amgakuin/javaclasscr30/controller/TopPage.java
+++ src/main/java/jp/co/amgakuin/javaclasscr30/controller/TopPage.java
@@ -3,10 +3,16 @@ package jp.co.amgakuin.javaclasscr30.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+/**
+ * コントローラクラス
+ */
 @Controller
 public class TopPage {
 	
+	
+	/**
+	 * インデックス表示
+	 */
 	@GetMapping("/")
 	private String index(Model model) {
 		model.addAttribute("message", "Hello World!!");
