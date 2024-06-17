3aa55ef31c48816539be03bef7a7591e8cf84869
Hiroya Morozumi <123cr0013@tokyo.amg.ac.jp>
2024-06-17 14:45:14 +0900

    コメント追加

--- src/main/java/jp/co/amgakuin/javaclasscr30/WebApplication.java
+++ src/main/java/jp/co/amgakuin/javaclasscr30/WebApplication.java
@@ -3,9 +3,18 @@ package jp.co.amgakuin.javaclasscr30;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * メインクラス
+ * @author amg
+ *
+ */
 @SpringBootApplication
 public class WebApplication {
-
+	
+	/**
+	 * 起動時に実行されるメソッド
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
