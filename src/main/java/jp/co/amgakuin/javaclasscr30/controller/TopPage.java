e49ec0d27dd5d5690124d7a850083bc72eb64a85
123cr0007 <123cr0007@tokyo.amg.ac.jp>
2024-06-17 14:55:59 +0900

    トップページクラスにコメント追加

--- src/main/java/jp/co/amgakuin/javaclasscr30/controller/TopPage.java
+++ src/main/java/jp/co/amgakuin/javaclasscr30/controller/TopPage.java
@@ -4,8 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * トップページクラス
+ * @author amg
+ *
+ */
+
 @Controller
 public class TopPage {
+	
+	/**
+	 * Hallo World！
+	 * @param model
+	 * @return
+	 */
 	
 	@GetMapping("/")
 	private String index(Model model) {
