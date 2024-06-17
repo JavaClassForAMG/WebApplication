ff3395ee633d63cba50f74da594b5ed2cf3e77c8
Hiroya Morozumi <123cr0013@tokyo.amg.ac.jp>
2024-06-17 16:36:44 +0900

    ユーザー名の反映を実装

--- src/main/java/jp/co/amgakuin/javaclasscr30/controller/FormPage.java
+++ src/main/java/jp/co/amgakuin/javaclasscr30/controller/FormPage.java
@@ -1,6 +1,7 @@
 package jp.co.amgakuin.javaclasscr30.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,9 +10,11 @@ public class FormPage {
 
 	@PostMapping("/")
 	private String doPost(
+			Model model,
 			@RequestParam("username") String name
-			) {
-		System.out.println(name);
+			) 
+	{
+		model.addAttribute("name", name);
 		return "FormPage";
 	}
 }
